4c62195ca78b5f96c84aba81ae3333f26df7f1a5
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2020-12-02 22:40:23 -0200

    Commit feito para registrar o início do projeto:
    	- Criação do projeto.
    	- Implementação do projeto no repositório git.
    	- Adição de uma descrição genérica.

---
+++ src/cronometro/Cronometro.java
@@ -0,0 +1,27 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package cronometro;
+
+
+public class Cronometro {
+
+    
+    // MÉTODO PRINCIPAL DO PROGRAMA - [ MAIN ]:
+    public static void main(String[] args) {
+    
+        
+        
+    }
+    
+    
+}
+
+
+
+
