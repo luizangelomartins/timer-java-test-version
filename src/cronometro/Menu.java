7aa7102ae163ec3445dc87fb9863196036161ef1
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2020-12-02 22:47:58 -0200

    Commit feito para registrar alterações no projeto:
    	- Criação do menu geral.
    	- Criação da classe que irá cronometrar.

---
+++ src/cronometro/Menu.java
@@ -0,0 +1,19 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package cronometro;
+
+
+// MÉTODO PRINCIPAL DA CLASSE:
+public class Menu {
+    
+}
+
+
+
+
---
+++ src/funcoes/Cronometrar.java
@@ -0,0 +1,19 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package funcoes;
+
+
+// MÉTODO PRINCIPAL DA CLASSE:
+public class Cronometrar {
+    
+}
+
+
+
+
